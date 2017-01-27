33847de9005e1f97c11265c0ecf2acbeea9955d2
Marko S <Marko S>
2017-01-27 06:46:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# What is this?
+Working example of integrating Ionic 1.x side menus with [ezAR](ezartech.com)
+
+Ezar implemention is based on [ezar-ionic](https://github.com/ezartech/ezar-ionic). Follow the instructions there to see how to install plugins.
+
+A lot of workarounds, and a lot of work that needs to be done, but this is just a basic example of usage with Ionic 1.x and side menus.
---
+++ www/js/controllers.js
@@ -0,0 +1,71 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {})
+
+.controller('HomeCtrl', function($scope) {
+
+})
+
+.controller("AppController", function($scope, $timeout) {
+
+    $scope.snapshotTimestamp = Date.now();
+    $scope.reverseCameraTimestamp = Date.now();
+
+    $scope.snapshot = function() {
+        //ignore ghost clicks, wait 1.5 sec between invocations
+        if (Date.now() - $scope.snapshotTimestamp < 1500) return;
+        $scope.snapshotTimestamp = Date.now();
+
+        //get snapshot & revcamera buttons to hide/show
+        var snapshotBtn = document.getElementById("snapshot");
+        var revCameraBtn = document.getElementById("revcamera");
+
+        var inclWebView = true; // include/exclude webView content on top of cameraView
+        var inclCameraBtns = true; // show/hide snapshot & revcamera btns
+
+        if (inclWebView && !inclCameraBtns) {
+            revCameraBtn.classList.add("hide");
+            snapshotBtn.classList.add("hide");
+        }
+
+        setTimeout(function() {
+            ezar.snapshot(
+                function() {
+                    //perform screen capture
+                    //show snapshot button
+                    if (inclWebView && !inclCameraBtns) {
+                        snapshotBtn.classList.remove("hide");
+                        revCameraBtn.classList.remove("hide");
+                    }
+                }, null, {
+                    encodingType: ezar.ImageEncoding.PNG,
+                    includeWebView: inclWebView,
+                    saveToPhotoAlbum: true
+                });
+        }, 200);
+    };
+
+    $scope.reverseCamera = function() {
+        //ignore ghost clicks, wait 1.5 sec between invocations
+        if (Date.now() - $scope.reverseCameraTimestamp < 1500) return;
+        $scope.reverseCameraTimestamp = Date.now();
+
+        var camera = ezar.getActiveCamera();
+        if (!camera) {
+            return; //no camera running; do nothing
+        }
+
+        var newCamera = camera;
+        if (camera.getPosition() == "BACK" && ezar.hasFrontCamera()) {
+            newCamera = ezar.getFrontCamera();
+        } else if (camera.getPosition() == "FRONT" && ezar.hasBackCamera()) {
+            newCamera = ezar.getBackCamera();
+        }
+
+        if (newCamera) {
+            newCamera.start();
+        }
+    }
+});
+
+;
